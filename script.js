802536b648a10ffe1e308acb50d0521329d40de1
Steven Dean <sdlanglois1@gmail.com>
2020-04-02 16:55:58 -0400

    second

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ $( document ).ready(function() {
         console.log (inputText,time)
         localStorage.setItem(time, inputText);
       });
+
+      var currentHour = parseInt((moment().format('h a')).split(" ")[0]);
+        console.log(currentHour);
+    var amPm = (moment().format('h a')).split(" ")[1];
+        console.log(amPm);
+        if (amPm == "pm") {
+            currentHour = currentHour + 12;
+            console.log(currentHour);
+        }
+
+      $(".hours").each(function() {
+            var hourBlock = parseInt(($(this).attr("id")).substring(4));
+            console.log(hourBlock)
+        if (currentHour > hourBlock) {
+            $(this).addClass("past");
+        }
+        else if (currentHour == hourBlock) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("future");
+        }
+      })
+    
+      console.log(moment().format('h a'));
 });
 
 
